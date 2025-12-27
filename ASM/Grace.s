9a5dc6d9f5e5b7c65d1f8477d9d71ffc8d216262
Tsubasa Ishikawa <tsishika@c2r9s2.42tokyo.jp>
2025-12-27 10:40:08 +0900

    微修正

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,37 +1,35 @@
 ; Only one comment
 
-%define SYS_OPEN   2
-%define SYS_CLOSE  3
-
-%define O_WRONLY   1
-%define O_CREAT    64
-%define O_TRUNC    512
-%define FLAGS      (O_WRONLY | O_CREAT | O_TRUNC)
-%define MODE_644   420
+%define NL 10
+%define DQ 34
+%define OUTFILE "Grace_kid.s"
 
 global main
 extern dprintf
 
 section .rodata
-output   db "Grace_kid.s", 0
-code_str db "; Only one comment%1$c%1$c%%define SYS_OPEN   2%1$c%%define SYS_CLOSE  3%1$c%1$c%%define O_WRONLY   1%1$c%%define O_CREAT    64%1$c%%define O_TRUNC    512%1$c%%define FLAGS      (O_WRONLY | O_CREAT | O_TRUNC)%1$c%%define MODE_644   420%1$c%1$cglobal main%1$cextern dprintf%1$c%1$csection .rodata%1$coutput   db %2$cGrace_kid.s%2$c, 0%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c  enter 0, 0%1$c  mov eax, SYS_OPEN%1$c  lea rdi, [rel output]%1$c  mov esi, FLAGS%1$c  mov edx, MODE_644%1$c  syscall%1$c  mov edi, eax%1$c  lea rsi, [rel code_str]%1$c  mov edx, 10%1$c  mov ecx, 34%1$c  lea r8,  [rel code_str]%1$c  xor eax, eax%1$c  call dprintf wrt ..plt%1$c  mov eax, SYS_CLOSE%1$c  syscall%1$c  leave%1$c  ret%1$c", 0
+output   db OUTFILE, 0
+code_str db "; Only one comment%1$c%1$c%%define NL 10%1$c%%define DQ 34%1$c%%define OUTFILE %2$cGrace_kid.s%2$c%1$c%1$cglobal main%1$cextern dprintf%1$c%1$csection .rodata%1$coutput   db OUTFILE, 0%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c  push rbx%1$c  mov eax, 2%1$c  lea rdi, [rel output]%1$c  mov esi, 577%1$c  mov edx, 420%1$c  syscall%1$c  mov ebx, eax%1$c  mov edi, eax%1$c  lea rsi, [rel code_str]%1$c  mov edx, NL%1$c  mov ecx, DQ%1$c  lea r8,  [rel code_str]%1$c  xor eax, eax%1$c  call dprintf wrt ..plt%1$c  mov eax, 3%1$c  mov edi, ebx%1$c  syscall%1$c  pop rbx%1$c  xor eax, eax%1$c  ret%1$c", 0
 
 section .text
 main:
-  enter 0, 0
-  mov eax, SYS_OPEN
+  push rbx
+  mov eax, 2
   lea rdi, [rel output]
-  mov esi, FLAGS
-  mov edx, MODE_644
+  mov esi, 577
+  mov edx, 420
   syscall
+  mov ebx, eax
   mov edi, eax
   lea rsi, [rel code_str]
-  mov edx, 10
-  mov ecx, 34
+  mov edx, NL
+  mov ecx, DQ
   lea r8,  [rel code_str]
   xor eax, eax
   call dprintf wrt ..plt
-  mov eax, SYS_CLOSE
+  mov eax, 3
+  mov edi, ebx
   syscall
-  leave
+  pop rbx
+  xor eax, eax
   ret
--- C/Sully.c
+++ C/Sully.c
@@ -19,7 +19,7 @@ int main(void) {
 
   snprintf(filename, sizeof(filename), "Sully_%d.c", x);
   snprintf(compile, sizeof(compile),
-   "clang -Wall -Wextra -Werror %s -o Sully_%d",
+   "cc -Wall -Wextra -Werror %s -o Sully_%d",
    filename, x);
   snprintf(execute, sizeof(execute), "./Sully_%d", x);
 
@@ -27,7 +27,7 @@ int main(void) {
   if (f < 0)
   return 1;
 
-  char *c = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$cint main(void) {%1$c  int x = %4$d;%1$c%1$c  if (strchr(__FILE__, '_') != NULL)%1$c  x--; %1$c%1$c  if (x < 0)%1$c  return 0;%1$c%1$c  char filename[64];%1$c  char compile[160];%1$c  char execute[64];%1$c%1$c  snprintf(filename, sizeof(filename), %2$cSully_%%d.c%2$c, x);%1$c  snprintf(compile, sizeof(compile), %2$cclang -Wall -Wextra -Werror %%s -o Sully_%%d%2$c, filename, x);%1$c  snprintf(execute, sizeof(execute), %2$c./Sully_%%d%2$c, x);%1$c%1$c  int f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%1$c  if (f < 0)%1$c  return 1;%1$c%1$c  char *c = %2$c%3$s%2$c;%1$c  dprintf(f, c, 10, 34, c, x);%1$c  close(f);%1$c%1$c  system(compile);%1$c  if (x >= 0)%1$c  system(execute);%1$c%1$c  return 0;%1$c}%1$c";
+  char *c = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$cint main(void) {%1$c  int x = %4$d;%1$c%1$c  if (strchr(__FILE__, '_') != NULL)%1$c  x--; %1$c%1$c  if (x < 0)%1$c  return 0;%1$c%1$c  char filename[64];%1$c  char compile[160];%1$c  char execute[64];%1$c%1$c  snprintf(filename, sizeof(filename), %2$cSully_%%d.c%2$c, x);%1$c  snprintf(compile, sizeof(compile), %2$ccc -Wall -Wextra -Werror %%s -o Sully_%%d%2$c, filename, x);%1$c  snprintf(execute, sizeof(execute), %2$c./Sully_%%d%2$c, x);%1$c%1$c  int f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%1$c  if (f < 0)%1$c  return 1;%1$c%1$c  char *c = %2$c%3$s%2$c;%1$c  dprintf(f, c, 10, 34, c, x);%1$c  close(f);%1$c%1$c  system(compile);%1$c  if (x >= 0)%1$c  system(execute);%1$c%1$c  return 0;%1$c}%1$c";
 
   dprintf(f, c, 10, 34, c, x);
   close(f);
