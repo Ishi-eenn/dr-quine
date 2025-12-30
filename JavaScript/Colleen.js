8b3dead31e7689bde5bfec243e76070b830084f2
Ishi_eenn <syi378039@gmail.com>
2025-12-30 10:29:14 +0900

    jsがダメで草

--- JavaScript/Colleen.js
+++ JavaScript/Colleen.js
@@ -5,12 +5,16 @@
  */
 
 const util = require("util");
-const s = "#!/usr/bin/env node\n\n/*\n * outside comment\n */\n\nconst util = require(\"util\");\nconst s = %j;\n\nfunction main() {\n  // inside comment\n\n  process.stdout.write(util.format(s, s));\n}\n\nmain();\n";
+const s = "#!/usr/bin/env node\n\n/*\n * outside comment\n */\n\nconst util = require(\"util\");\nconst s = %j;\n\nfunction printSelf() {\n  process.stdout.write(util.format(s, s));\n}\n\nfunction main() {\n  // inside comment\n\n  printSelf();\n}\n\nmain();\n";
+
+function printSelf() {
+  process.stdout.write(util.format(s, s));
+}
 
 function main() {
   // inside comment
 
-  process.stdout.write(util.format(s, s));
+  printSelf();
 }
 
 main();
