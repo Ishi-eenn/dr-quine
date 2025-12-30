9b2e2868045101c9f68b3842121231a9aa19110b
Ishi_eenn <syi378039@gmail.com>
2025-12-30 10:15:40 +0900

    これで差分は出ないはず

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -4,17 +4,23 @@ global main
 extern printf
 
 section .rodata
-code_str db "; Outside comment%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c  enter 0, 0%1$c  lea rdi, [rel code_str]%1$c  ; Inside comment%1$c  mov rsi, 10%1$c  mov rdx, 34%1$c  lea rcx, [rel code_str]%1$c  xor eax, eax%1$c  call printf wrt ..plt%1$c  leave%1$c  ret%1$c", 0
+s db "; Outside comment%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cprint_source:%1$c  enter 0, 0%1$c  lea rdi, [rel s]%1$c  mov rsi, 10%1$c  mov rdx, 34%1$c  lea rcx, [rel s]%1$c  xor eax, eax%1$c  call printf wrt ..plt%1$c  leave%1$c  ret%1$c%1$cmain:%1$c  enter 0, 0%1$c  ; Inside comment%1$c  call print_source%1$c  leave%1$c  ret%1$c", 0
 
 section .text
-main:
+print_source:
   enter 0, 0
-  lea rdi, [rel code_str]
-  ; Inside comment
+  lea rdi, [rel s]
   mov rsi, 10
   mov rdx, 34
-  lea rcx, [rel code_str]
+  lea rcx, [rel s]
   xor eax, eax
   call printf wrt ..plt
+  leave
+  ret
+
+main:
+  enter 0, 0
+  ; Inside comment
+  call print_source
   leave
   ret
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -9,7 +9,7 @@ extern dprintf
 
 section .rodata
 output   db OUTFILE, 0
-code_str db "; Only one comment%1$c%1$c%%define NL 10%1$c%%define DQ 34%1$c%%define OUTFILE %2$cGrace_kid.s%2$c%1$c%1$cglobal main%1$cextern dprintf%1$c%1$csection .rodata%1$coutput   db OUTFILE, 0%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c  push rbx%1$c  mov eax, 2%1$c  lea rdi, [rel output]%1$c  mov esi, 577%1$c  mov edx, 420%1$c  syscall%1$c  mov ebx, eax%1$c  mov edi, eax%1$c  lea rsi, [rel code_str]%1$c  mov edx, NL%1$c  mov ecx, DQ%1$c  lea r8,  [rel code_str]%1$c  xor eax, eax%1$c  call dprintf wrt ..plt%1$c  mov eax, 3%1$c  mov edi, ebx%1$c  syscall%1$c  pop rbx%1$c  xor eax, eax%1$c  ret%1$c", 0
+s db "; Only one comment%1$c%1$c%%define NL 10%1$c%%define DQ 34%1$c%%define OUTFILE %2$cGrace_kid.s%2$c%1$c%1$cglobal main%1$cextern dprintf%1$c%1$csection .rodata%1$coutput   db OUTFILE, 0%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cmain:%1$c  push rbx%1$c  mov eax, 2%1$c  lea rdi, [rel output]%1$c  mov esi, 577%1$c  mov edx, 420%1$c  syscall%1$c  mov ebx, eax%1$c  mov edi, eax%1$c  lea rsi, [rel s]%1$c  mov edx, NL%1$c  mov ecx, DQ%1$c  lea r8,  [rel s]%1$c  xor eax, eax%1$c  call dprintf wrt ..plt%1$c  mov eax, 3%1$c  mov edi, ebx%1$c  syscall%1$c  pop rbx%1$c  xor eax, eax%1$c  ret%1$c", 0
 
 section .text
 main:
@@ -21,10 +21,10 @@ main:
   syscall
   mov ebx, eax
   mov edi, eax
-  lea rsi, [rel code_str]
+  lea rsi, [rel s]
   mov edx, NL
   mov ecx, DQ
-  lea r8,  [rel code_str]
+  lea r8,  [rel s]
   xor eax, eax
   call dprintf wrt ..plt
   mov eax, 3
